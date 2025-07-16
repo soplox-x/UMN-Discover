aeb8923949502a34c0f0bb501cc1654a4fd67f13
Lyvern <tarbo.sk@gmail.com>
2025-07-16 12:12:09 -0500

    calendar is able to switch views now

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import React, { useState, useCallback } from "react";
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment'
 import '../styles/Calendar.css'
 import '../styles/react-big-calendar.css'
@@ -7,24 +7,68 @@ import '../styles/react-big-calendar.css'
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment)
 
+const now = new Date();
+
+const events = [
+    {
+        id: 0,
+        title: "Today",
+        start: new Date(new Date().setHours(new Date().getHours() - 3)),
+        end: new Date(new Date().setHours(new Date().getHours() + 3))
+    },
+    {
+        id: 1,
+        title: "Point in Time Event",
+        start: now,
+        end: now
+    }
+];
+
+
 function CalendarPage() {
-    // state = {
-    //     events: [
-    //       {
-    //         start: moment().toDate(),
-    //         end: moment().add(1, "days").toDate()
-    //       }
-    //     ]
-    // };
+
+    const [date, setDate] = useState(now);
+    const [view, setView] = useState(Views.MONTH);
+
+    // const onNavigate = useCallback((newDate) => setDate(newDate), [setDate]);
+    const onNavigate = (newDate) => setDate(newDate);
+
+    const onView = (newView) => setView(newView);
+    // const onView = useCallback((newView) => setView(newView), [setView]);
+
+
+    const [eventsData, setEventsData] = useState(events);
+
+
+    const handleSelect = ({ start, end }) => {
+        console.log(start);
+        console.log(end);
+        const title = window.prompt("New Event name");
+        if (title)
+          setEventsData([
+            ...eventsData,
+            {
+              start,
+              end,
+              title
+            }
+          ]);
+    };
     
     return (
         <div className='calendar'>
             <Calendar 
-                views={["day", "agenda", "work_week", "month"]}
+                views={["day", "work_week", "month"]}
+                view={view}
+                date={date}
+                onNavigate={onNavigate}
+                onView={onView}
+                //selectable
                 localizer={localizer}
                 defaultDate={new Date()}
                 defaultView="month"
-                style={{ height: "90vh" }}
+                style={{ height: "80vh" }}
+                //events={eventsData}
             />
         </div>
     );
