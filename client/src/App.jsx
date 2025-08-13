028d5b49af933e4dfa37476dc960ea7524b4a4d2
NAMERIO <natanberhanu22@gmail.com>
2025-08-12 19:03:22 -0500

    added privacy page using https://www.privacypolicytemplate.net/ templet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Calendar from './components/Calendar';
 import GradeSearch from './components/GradeSearch';
@@ -16,6 +16,16 @@ import StudySpots from './components/study_spot_code/StudySpots';
 import EastBankPage from './components/study_spot_code/EastBankPage';
 import StPaulPage from './components/study_spot_code/StPaulPage';
 import WestBankPage from './components/study_spot_code/WestBankPage';
+import Privacy from './components/Privacy';
+import './styles/Privacy.css';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 
 function App() {
@@ -130,6 +140,7 @@ function App() {
   }
   return (
     <Router>
+      <ScrollToTop />
       <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
         <Header
           darkMode={darkMode}
@@ -152,6 +163,7 @@ function App() {
           <Route path="/studyspots/westbank" element={<WestBankPage />} />
           <Route path="/studyspots/eastbank" element={<EastBankPage />} />
           <Route path="/studyspots/stpaul" element={<StPaulPage />} />
+          <Route path="/privacy" element={<Privacy />} />
           <Route path="*" element={<HomePage darkMode={darkMode} setDarkMode={setDarkMode} />} />
         </Routes>
         <Footer />
