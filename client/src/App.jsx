78a5565f6095f434e5a30924dfd6f3d57abe5c01
Anthony K <anthonykrahmer@gmail.com>
2025-08-19 21:34:32 -0500

    EXTREMELY basic map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Calendar from './components/Calendar'; 
 import GradeSearch from './components/GradeSearch';
+import Map from './components/Map';
 import CourseDetail from './components/CourseDetail';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -26,6 +27,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage darkMode={darkMode} setDarkMode={setDarkMode} />} />
           <Route path="/calendar" element={<Calendar />} />
+          <Route path="/map" element={<Map />} />
           <Route path="/grades" element={<GradeSearch />} />
           <Route path="/course/:courseId" element={<CourseDetail />} />
           <Route path="*" element={<HomePage darkMode={darkMode} setDarkMode={setDarkMode} />} />
